d4a8b4e2d6f01ac5cb097481df9ed7154407cf9b
brthngnsh <demongod250299@gmail.com>
2019-08-10 13:36:18 +0530

    removed recent duplicate deletion program for linked list

--- Linked Lists/remove_duplicates_in_list.py
+++
@@ -1,9 +0,0 @@
-def remove_dups_in_list(L):
-    it = L
-    while it:
-        next_distinct = it.next
-        while next_distinct and next_distinct.data == it.data:
-            next_distinct = next_distinct.next
-        it.next = next_distinct
-        it = it.next
-    return L
