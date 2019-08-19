de3fc2771027d1a22f5d358494cc1c18d0ec76b9
brthngnsh <demongod250299@gmail.com>
2019-08-19 15:41:15 +0530

    stack __init__

---
+++ Stack/stack_init.py
@@ -0,0 +1,25 @@
+#This file contains only Stack initialization
+
+import collections
+class Stack :
+    ElementWithCachedMax = collections.namedtuple('ElementWithCachedMax',('element','max'))
+
+    def __init__(self, *args, **kwargs):
+        self._element_with_cached_max = []
+    
+    def empty(self) :
+        return len(_element_with_cached_max) == 0
+
+    def max(self) :
+        if self.empty() :
+            raise IndexError('Index Error :: Empty List')
+        return self._element_with_cached_max[-1].max
+    
+    def pop(self) :
+        if self.empty() :
+            raise IndexError('pop() : Empty stack')
+        return self._element_with_cached_max.pop().element
+
+    def push(self,x):
+        self._element_with_cached_max.append(self._element_with_cached_max(x,x if self.empyt() else max(x,self.max())))
+
